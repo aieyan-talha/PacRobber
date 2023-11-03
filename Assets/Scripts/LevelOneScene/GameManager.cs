aad3c4645a5bce322600955fda64afd6e27e08ba
aieyan-talha <52277557+aieyan-talha@users.noreply.github.com>
2023-11-03 19:56:47 +1100

    Setup movement controller

--- Assets/Scripts/LevelOneScene/GameManager.cs
+++ Assets/Scripts/LevelOneScene/GameManager.cs
@@ -1,18 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Linq;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    public GameObject robberLifePrefab;
+    public int robberLives;
+    public Canvas canvas;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 startPosition = new Vector3(-680, -460, 0);
+
+        for (int i = 0; i < robberLives; i++)
+        {
+            GameObject robberLifeInstance = Instantiate(robberLifePrefab);
+
+            robberLifeInstance.transform.SetParent(canvas.transform, false);
+
+            robberLifeInstance.transform.localPosition = startPosition + new Vector3(i * 50, 0, 0);
+        }
+
+        //Hide the ghost timer on start of the game
+        HideTextWithTag("GhostTimerLabel");
+        HideTextWithTag("GhostTimerValue");
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void HideTextWithTag(string tag)
+    {
+        TextMeshProUGUI[] textObjects = GameObject.FindGameObjectsWithTag(tag)
+            .Select(go => go.GetComponent<TextMeshProUGUI>())
+            .Where(text => text != null)
+            .ToArray();
+
+        foreach (TextMeshProUGUI textComponent in textObjects)
+        {
+            textComponent.enabled = false;
+        }
+    }
+
+    public void ShowTextWithTag(string tag)
+    {
+        TextMeshProUGUI[] textObjects = GameObject.FindGameObjectsWithTag(tag)
+            .Select(go => go.GetComponent<TextMeshProUGUI>())
+            .Where(text => text != null)
+            .ToArray();
+
+        foreach (TextMeshProUGUI textComponent in textObjects)
+        {
+            textComponent.enabled = true;
+        }
     }
 }
---
+++ Assets/Scripts/LevelOneScene/PacStudentController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacStudentController : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    private Vector3 currentTargetPosition;
+    private Vector3 nextTargetPosition;
+    private bool isMoving = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
